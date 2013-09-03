70c52d9ee8110f8e1d523e1aab17c56990d59c23
Peter McWilliams <petemcw@gmail.com>
2013-09-03 17:21:41 -0500

    Clean up comment documentation

--- api/sendthestyle.rb
+++ api/sendthestyle.rb
@@ -19,43 +19,62 @@ module Api
       Compass.add_project_configuration("config/compass.rb")
     end
 
-    get "/?" do
-      json({ message: "Welcome to Send-The-Styles!" })
-    end
-
     ## handle 404 errors
     not_found do
       halt_404_not_found
     end
 
+    ##
+    # Homepage request; generates simple welcome message
+    #
+    # GET /
+    #
+    get "/?" do
+      json({ message: "Welcome to Send-The-Styles!" })
+    end
+
+    ##
+    # Create namespace to group all API methods
+    #
     namespace "/api" do
+
+      ##
+      # Invalid request for API namespace alone
+      #
+      # GET /api(/)
+      #
       get "/?" do
         halt_400_bad_request
       end
 
+      ##
+      # Compile passed SASS into CSS, respecting any passed options
+      #
+      # GET /api/compile?file=FQDN(&option=value)
+      #
       get "/compile/?" do
         # attempt to download the remote SASS file for processing
         style_file = params[:file]
-
         halt_400_bad_request("Invalid SASS file") \
           unless Faraday.head(style_file).status == 200
-
         response = Faraday.get(style_file)
 
         begin
+          # set any passed Compass options
           compass_params = whitelist(params, VALID_PARAMS)
-
           Compass.configuration do |config|
             compass_params.each do |key, value|
               config.send("#{key}=", value)
             end
           end
 
-          css = send(:scss, response.body.chomp, Compass.sass_engine_options.merge!({style: :expanded, line_comments: false}))
+          # pass the downloaded SASS content to the renderer
+          css = send(:scss, response.body.chomp)
         rescue Sass::SyntaxError => e
           halt_400_bad_request e.to_s
         end
 
+        # return generated CSS
         json({
           code: "compile_success",
           css:  css
